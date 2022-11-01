bb3e60e529584b294bdefee93471ca676b1c72c1
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-01 00:45:15 -0300

    first commit console app

---
+++ helpers/inquirer.js
@@ -0,0 +1,183 @@
+import inquirer from "inquirer";
+import Color from 'colors'
+
+
+
+
+
+const question = [
+  {
+    type: "list",
+    name: "option",
+    message: "what do you want to do?\n",
+    choices: [
+      {
+        value: "1",
+        name: `${'1.'.green} Create todo`,
+      },
+      {
+        value: "2",
+        name: `${'2.'.green} List Todos`,
+      },
+      {
+        value: "3",
+        name: `${'3.'.green} List todos completed`,
+      },
+      {
+        value: "4",
+        name: `${'4.'.green} List todos pending`,
+      },
+      {
+        value: "5",
+        name: `${'5.'.green} Complete todos(s)`,
+      },
+      {
+        value: "6",
+        name: `${'6.'.green} Delete todo`,
+      },
+      {
+        value: "0",
+        name: `${'7.'.green} Exit`,
+      },
+    ],
+  },
+];
+
+export const menu = async () => {
+
+  console.clear();
+
+  console.log("==========================".green);
+  console.log("          Menu            ");
+  console.log("==========================\n".green);
+
+  const { option } = await inquirer.prompt(question);
+
+  return option;
+};
+
+export const pause = async () => {
+
+  const questionContinue = [
+    {
+      type: "input",
+      name: "desc",
+      message: "Press ENTER to continue".green,
+    },
+  ];
+  console.log("\n");
+
+  await inquirer.prompt(questionContinue);
+};
+
+export const readInput = async (message) => {
+
+  console.clear()
+
+  const question = [
+
+    {
+      type: "input",
+      name: "description",
+      message,
+
+      validate(value) {
+
+        if (value.length === 0) {
+
+          return "Please write todo";
+        } else {
+          return true;
+        }
+      },
+    },
+  ];
+
+  const { description } = await inquirer.prompt(question)
+
+  return description;
+};
+
+export const listDeleteTodos = async (todos = []) => {
+
+  const choices = todos.map((todo, i) => {
+    const index = i + 1
+    return {
+
+      value: todo.id,
+      name: `${index} ${todo.description}`
+    }
+
+  })
+  choices.unshift({
+    value: '0', name: '0' + ' ' + 'Cancel'.red
+  })
+  const question = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'choose todo to delete\n',
+      choices
+    }
+  ]
+
+  const { id } = await inquirer.prompt(question);
+  return id
+
+
+
+  // {
+  //   value: "1",
+  //   name: `${'1.'.green} Create todo`,
+  // }
+
+
+}
+export const confirm = async (message) => {
+
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message,
+
+    }
+  ]
+  const { ok } = await inquirer.prompt(question);
+  return ok
+
+}
+export const checkList = async (todos = []) => {
+
+  const choices = todos.map((todo, i) => {
+    const index = i + 1
+    return {
+
+      value: todo.id,
+      name: `${index} ${todo.description}`,
+      checked: (todo.createdDate) ? true : false
+    }
+
+  })
+ 
+  const question = [
+    {
+      type: 'checkbox',
+      name: 'ids',
+      message: 'choose to check\n',
+      choices
+    }
+  ]
+
+  const { ids } = await inquirer.prompt(question);
+  return ids
+
+
+
+  // {
+  //   value: "1",
+  //   name: `${'1.'.green} Create todo`,
+  // }
+
+
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+import { readInput } from "./helpers/inquirer.js";
+
+const main = async () => {
+  const text = await readInput("Hola: ");
+  console.log(text)
+};
+
+main();
+
